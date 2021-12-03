f127271d6dab10a26743eada6ce27e2c6271f020
sosoYim <sohyung.yim@gmail.com>
2021-12-03 17:10:26 +0900

    doc: Initialize work space
    - Seperate server & client
    - babel, eslint, live-server etc

---
+++ shoes-shop/.eslintrc.cjs
@@ -0,0 +1,28 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  globals: {
+    jest: true,
+  },
+  extends: ['eslint:recommended', 'plugin:react/recommended'],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 13,
+    sourceType: 'module',
+  },
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
+  plugins: ['react'],
+  rules: {
+    'no-unused-vars': 'warn',
+    'react/prop-types': 'off',
+  },
+};
